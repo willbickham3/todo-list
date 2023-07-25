f4c8ece17dfe3c9ba7ed19f8d9bf334e1368e9ec
Will <willbickham3@gmail.com>
2023-07-24 22:23:36 -0400

    Add optional case for id; comment explanation of function

--- src/domManipulation/elementCreater.js
+++ src/domManipulation/elementCreater.js
@@ -1,6 +1,11 @@
-function createAnElement(element, classList, string) {
+// createAnElement takes 4 arguments (element that you want to create, class name, id (optional: can be set to null), string for innerHTML)
+
+function createAnElement(element, classList, id, string) {
     const newEle = document.createElement(element);
     newEle.classList.add(classList);
+    if (id) {
+        newEle.setAttribute('id', id);
+    }
     newEle.innerHTML = string;
 
     return newEle
