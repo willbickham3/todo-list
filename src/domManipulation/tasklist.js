c2e6f6cf4943055e3952a8147265b86f250cf800
Will <willbickham3@gmail.com>
2023-07-25 21:36:08 -0400

    Write function to create the navigation divs of page

---
+++ src/domManipulation/tasklist.js
@@ -0,0 +1,14 @@
+// createAnElement takes 4 arguments (element that you want to create, class name, id (optional: can be set to null), string for innerHTML)
+
+import createAnElement from "./elementCreater";
+
+function tasklist() {
+    const navContainer = document.querySelector('.nav');
+    
+    const navBar = createAnElement('div', 'nav-bar', null, null);
+    const projectContainer = createAnElement('div', 'project-container', null, null);
+
+    navContainer.append(navBar, projectContainer);
+}
+
+export default tasklist
