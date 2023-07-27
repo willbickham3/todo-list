6db7160b25b6d85ecc181bd6a0b57244fb639499
Will <willbickham3@gmail.com>
2023-07-27 14:27:38 -0400

    Place a lot of the code in a class for easier iteration

--- src/domManipulation/addATask.js
+++ src/domManipulation/addATask.js
@@ -1,58 +1,28 @@
 import createAnElement from "../domManipulation/elementCreater";
-import { differenceInDays, differenceInHours, format } from "date-fns";
+import task from "../appLogic/task";
 
 
 function addATask(button) {
         button.addEventListener('click', () => {
             let taskInput = document.querySelector('.taskInput');
-            let dateInput = document.querySelector('.dateInput')
+            let dateInput = document.querySelector('.dateInput');
 
-            if (taskInput.value.trim() === '')
-            {return}
+            const newTask = new task(taskInput.value, dateInput.value);
+
 
             // Container under fieldset for tasks
-
-            const divForTasks = document.createElement('div');
-            divForTasks.classList.add('actual-container');
-
-            // Actual title of task
-
-            const taskDiv = document.createElement('div');
-            taskDiv.classList.add('task');
-            taskDiv.innerHTML = taskInput.value;
+            const divForTasks = createAnElement('div', 'actual-container', null, null);
 
             // Button for deleting tasks
 
-            const deleteBtn = document.createElement('button');
-            deleteBtn.classList.add('delete');
-            deleteBtn.innerHTML = "X";
-
-            const date = new Date();
-            const formatDate = new Date(dateInput.value);
-            const dueDate = differenceInDays(formatDate, date)
-            const hoursDifference = differenceInHours(formatDate, date);
-
-            const dueDateValue = document.createElement('div');
-            dueDateValue.classList.add('due-date')
-            if (dateInput.value == '') {
-                dueDateValue.innerHTML = "";
-            }
-
-            else if (dueDate < 1) {
-                dueDateValue.innerHTML = `~${hoursDifference} Hours`
-            }
-            else if (dueDate >= 2) {
-                dueDateValue.innerHTML = `Coming up in ~${dueDate} days`
-            }
-            else {dueDateValue.innerHTML= `Coming up in ~${dueDate} day`;}
                 
             let taskContainer = document.querySelector('.to-do-list');
-            divForTasks.append(taskDiv, dueDateValue, deleteBtn);
             taskContainer.append(divForTasks);
             taskInput.value = "";
             dateInput.value = "";
-            console.log(divForTasks);
+            newTask.appendItAll();
+
         })
-    }
+        }
 
 export default addATask
