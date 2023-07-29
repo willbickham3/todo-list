d995da711dbbf786f9fa42e2d0c8697b05391fc0
Will <willbickham3@gmail.com>
2023-07-29 15:48:11 -0400

    Add projectInput

--- src/domManipulation/addATask.js
+++ src/domManipulation/addATask.js
@@ -4,8 +4,9 @@ function addATask(button) {
         button.addEventListener('click', () => {
             let taskInput = document.querySelector('.taskInput');
             let dateInput = document.querySelector('.dateInput');
+            let projectInput = document.querySelector('select');
 
-            const newTask = new Task(taskInput.value, dateInput.value);
+            const newTask = new Task(taskInput.value, dateInput.value, projectInput.value);
 
             if (taskInput.value == '') 
             {return}
