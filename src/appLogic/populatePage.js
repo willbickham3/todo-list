41a896a235f14539f7f8a9dcf16ae0fc89c79e6d
Will <willbickham3@gmail.com>
2023-07-30 14:31:40 -0400

    Function reads from localStorage and repopulates page on load

---
+++ src/appLogic/populatePage.js
@@ -0,0 +1,18 @@
+import Task from "./task";
+
+function populatePage() {
+    let retrievedTasks = localStorage.getItem('tasks');
+    if (retrievedTasks == []) {
+        return
+    }
+    else {
+    retrievedTasks = JSON.parse(retrievedTasks);
+    retrievedTasks.forEach(tasks => {
+        let task = new Task(tasks.title, tasks.dueDate);
+        task.appendItAll();
+    })
+}
+
+}
+
+export default populatePage
