f3360ac04320c7797c4ef0abbe6527e38ee85451
Will <willbickham3@gmail.com>
2023-07-31 15:20:42 -0400

    Write function that appends projects from localStorage to DOM

--- src/appLogic/populatePage.js
+++ src/appLogic/populatePage.js
@@ -1,8 +1,15 @@
 import Task from "./task";
+import Project from "./project";
 
 function populatePage() {
+    getTasks();
+    projectAppender();
+}
+
+
+function getTasks() {
     let retrievedTasks = localStorage.getItem('tasks');
-    if (retrievedTasks == []) {
+    if (retrievedTasks === null || retrievedTasks.length === 0) {
         return
     }
     else {
@@ -12,7 +19,20 @@ function populatePage() {
         task.appendItAll();
     })
 }
+}
 
+function projectAppender() {
+    let retrievedProjects = localStorage.getItem('projects');
+    if (retrievedProjects === null || retrievedProjects.length === 0) {
+        return
+    }
+    else {
+    retrievedProjects = JSON.parse(retrievedProjects);
+    retrievedProjects.forEach(project => {
+        let newProject = new Project(project);
+        newProject.projectSelector();
+        newProject.appendProject();
+    })}
 }
 
 export default populatePage
