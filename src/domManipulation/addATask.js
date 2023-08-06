f9d62c9810a6b5cc035d6b718aab3e21eaecf98a
Will <willbickham3@gmail.com>
2023-08-05 23:26:41 -0400

    Add checks for duplicate task names

--- src/domManipulation/addATask.js
+++ src/domManipulation/addATask.js
@@ -6,13 +6,21 @@ function addATask(button) {
             let dateInput = document.querySelector('.dateInput');
             let projectInput = document.querySelector('select');
 
-            const newTask = new Task(taskInput.value, dateInput.value, projectInput.value);
+            let taskManager = localStorage.getItem('tasks');
+            taskManager = JSON.parse(taskManager);
 
-            if (taskInput.value == '') 
-            {return}
-            taskInput.value = "";
-            dateInput.value = "";
-            newTask.appendItAll();
+            if (taskManager.some(tasks => tasks.title === taskInput.value)) {
+                alert('Title already exists! Please select a new one!');
+                return
+            }            
+            else {
+                let newTask = new Task(taskInput.value, dateInput.value, projectInput.value);
+                if (taskInput.value == '') 
+                {return}
+                taskInput.value = "";
+                dateInput.value = "";
+                newTask.appendItAll();
+            }
 })}
 
 export default addATask
